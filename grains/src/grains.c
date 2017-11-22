e839547764bd63b833caf9af6707cf6d40255dfa
REM <18474919+xREMx@users.noreply.github.com>
2017-11-22 13:09:37 -0800

    finished solution for grains problem

---
+++ grains/src/grains.c
@@ -0,0 +1,16 @@
+#include "grains.h"
+#include <math.h>
+
+
+unsigned long square (int squareNum) 
+{
+	if (squareNum == 1) { return 1; }
+	if (squareNum > 64) { return 0; }
+	return pow(2, squareNum - 1);
+}
+
+
+unsigned long total (void) 
+{
+	return 18446744073709551615ul;
+}
---
+++ grains/src/grains.h
@@ -0,0 +1,7 @@
+#ifndef GRAINS_H
+#define GRAINS_H
+
+unsigned long    square     (int squareNum);
+unsigned long    total      (void);
+
+#endif
