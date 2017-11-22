efd6d216a78ab485e3fa6dfda254312b564ef5c2
REM <18474919+xREMx@users.noreply.github.com>
2017-11-21 16:47:01 -0800

    deleted:    .README.md.swp 	modified:   src/raindrops.c 	deleted:    test/.test_raindrops.c.swp 	modified:   tests.out

--- raindrops/src/raindrops.c
+++ raindrops/src/raindrops.c
@@ -5,13 +5,9 @@
 
 void convert (char result[], int drops)
 {
-	for (int i = 1; i <= drops; i++) {
-		if (drops % i == 0) {
-			if (i == 3) { sprintf(result, "%s%s", result, "Pling"); }
-			if (i == 5) { sprintf(result, "%s%s", result, "Plang"); }
-			if (i == 7) { sprintf(result, "%s%s", result, "Plong"); }
-		}
-	}
+	if (drops % 3 == 0) { sprintf(result, "%s%s", result, "Pling"); }
+	if (drops % 5 == 0) { sprintf(result, "%s%s", result, "Plang"); }
+	if (drops % 7 == 0) { sprintf(result, "%s%s", result, "Plong"); }
 
 	if (strcmp(result, "") == 0) { sprintf(result, "%d", drops); }
 
