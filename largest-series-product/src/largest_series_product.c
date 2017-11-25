f1cc5791632738dec8dbf45b5e75683c57bc498a
REM <18474919+xREMx@users.noreply.github.com>
2017-11-25 00:08:22 -0800

    finished largest series project

--- largest-series-product/src/largest_series_product.c
+++ largest-series-product/src/largest_series_product.c
@@ -14,83 +14,59 @@ int64_t largest_series_product (const char* numbers, unsigned int count)
 	
 	if (count == 0) { return 1; }
 	if (strlen(numbers) < count) { return -1; }
-	//set up
-	if ((sum = setUpSeries(&pHead, &pTail, count)) == -1) { return 0; }
-	if (sum > largestSum) { largestSum = sum; }
+
+	if ((sum = setUpSeries(&pHead, &pTail, count)) < 0) { 
+		if (sum == -1) { return -1; }  //dissalowed char
+		if (sum == -2) { return 0; }  //could not create string
+	}
+	largestSum = sum;
 
 	while (*pTail != '\0') { 
 		if (*pTail == '0') {
 			pTail++;
 			pHead = pTail;
-			if ((sum = setUpSeries(&pHead, &pTail, count)) == -1) { break; }	
+			if ((sum = setUpSeries(&pHead, &pTail, count)) < 0 ) { 
+				if (sum == -1) { return -1; }  //dissalowed char
+				break;
+			}	
 		}
 		else {
 			sum /= *pHead - '0';
 			pHead++;
+			if(*pTail < '0' || *pTail > '9') { return -1; }
 			sum *= *pTail - '0';
 			pTail++;
 		}
 		if (sum > largestSum) { largestSum = sum; }
 	}
+
 	return largestSum;
-/*
-	//walk !through array
-	while (*pTail != '\0') {
-		while (pTail - pHead < count && *pTail != '\0') {
-			if (*pTail == '0') {
-				while (*pTail == '0') { pTail++; }
-				pHead = pTail;
-				if (*pTail != '\0') {
-					sum = *pHead - '0';
-					pTail++;
-				}
-			}
-			else {
-				sum *= *pTail - '0';
-				pTail++;
-			}
-		}
-		if (*pTail == '\0') { break; }
-		if (*pTail == '0') {
-			pHead = pTail;
-		}
-		else { 
-			sum /= *pHead - '0';
-			pHead++;
-			sum *= *pTail - '0';
-			pTail++;
-		}
-		if (sum > largestSum) { largestSum = sum; }
-	}
-	if (sum > largestSum) { largestSum = sum; }
-	return largestSum; 
-*/
 }
 
 int64_t setUpSeries (const char** pHead, const char** pTail, unsigned int count) 
 {
-	int64_t sum;
+	int64_t sum = 0;
 	*pHead = *pTail;
-	do {
-		sum = 1;
+	while (sum == 0) {
 		while (**pHead == '0') { 
 			*pTail += 1; 
 			*pHead = *pTail;
 			}
 
-		if (**pTail == '\0') { return -1; }
-		while (*pTail - *pHead < count && **pTail != '\0') {
-			if(**pTail < '0' || **pTail > '9') { return -1; }
+		sum = 1;
+		while (**pTail != '\0') {
+			if(**pTail < '0' || **pTail > '9') { return -1; }  //if we found a dissalowed char
 			sum *= **pTail - '0';
 			*pTail += 1;
+			if (*pTail - *pHead == count) { return sum; }
 			if (**pTail == '0') { 
 				*pTail += 1;
 				*pHead = *pTail;
 				sum = 0;
 				break;
 			}
-		if (*pTail - *pHead == count) { return sum; }
 		}
-	} while (sum == 0);
-	return -2;  //Major error, should never reach here
+	if (**pTail == '\0') { return -2; }  //if we were unable to make a full series
+	} 
+	return -11;  //Major error, should never reach here
 }
