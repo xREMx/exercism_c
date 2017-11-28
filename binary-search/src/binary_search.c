9fa0ded0a9dc7871d1e2928ffb90b6b7fe9fc735
REM <18474919+xREMx@users.noreply.github.com>
2017-11-28 14:46:55 -0800

    Converted to pointer implementation

--- binary-search/src/binary_search.c
+++ binary-search/src/binary_search.c
@@ -1,18 +1,17 @@
 #include "binary_search.h"
 
 
-int* binary_search (int key, int arr[], size_t length) {
+int32_t* binary_search (int32_t key, int32_t arr[], size_t length) {
 	if (arr == NULL) { return NULL; }
 	if (length <= 0) { return NULL; }
 
-	int iHead = 0;
-	int iTail = length - 1;
+	int32_t* iHead = arr;
+	int32_t* iTail = arr + length - 1;
 	
 	while (iHead <= iTail) {
-		int middle = (iHead + iTail) / 2;
-		int arrCur = arr[middle];
-		if (arrCur == key) { return &arr[middle]; }
-		else if (arrCur > key) { iTail = middle - 1; }
+		int32_t* middle = iHead + (iTail - iHead) / 2;
+		if (*middle == key) { return middle; }
+		else if (*middle > key) { iTail = middle - 1; }
 		else { iHead = middle + 1; }
 	}
 	return NULL;
--- binary-search/src/binary_search.h
+++ binary-search/src/binary_search.h
@@ -2,7 +2,8 @@
 #define BINARY_SEARCH_H
 
 #include <stddef.h>
+#include <stdint.h>
 
-int* binary_search (int key, int arr[], size_t length);
+int32_t* binary_search (int32_t key, int32_t arr[], size_t length);
 
 #endif
